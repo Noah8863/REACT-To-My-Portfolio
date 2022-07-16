cda07e838da1c89ad3d02e6384ab7897113fb7bb
Noah Hoffman <stacknoah99@gmail.com>
2022-07-16 16:28:23 -0600

    got all the projects to rotate when clicked. Added comments on future features

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,28 +1,50 @@
 import React from 'react';
+import {useState} from 'react';
 import project1 from '../images/JATE-Pic.jpg';
 import project2 from '../images/Coffee:Book-Pic.jpg';
 import project3 from '../images/Password_Gen_Pic.png'
 
+// const background = document.getElementById('projects')
 
 function projects() {
+
+    // const parking = () => {
+    //     background.classList.remove('coffee-back-drop')
+    //     background.classList.remove('password-back-drop')
+    //     background.classList.add('car-back-drop')
+    // }
+    // const coffee = () => {
+    //     background.classList.remove('car-back-drop')
+    //     background.classList.remove('password-back-drop')
+    //     background.classList.add('coffee-back-drop')
+    // }
+    // const password = () => {
+    //     background.classList.remove('car-back-drop')
+    //     background.classList.remove('coffee-back-drop')
+    //     background.classList.add('password-back-drop')
+    // }
+
+    //Need to figure how to change the background onClick for each project
+    //Change onclick to onClick and make it an object
+
     return (
         <section className="projectSection car-back-drop" id="projects">
-            <div class="container">
-                <input type="radio" name="slider" id="item-1" onclick="parking()" checked></input>
-                <input type="radio" name="slider" id="item-2" onclick="coffee()" ></input>
-                <input type="radio" name="slider" id="item-3" onclick="password()"></input>
+            <div className="container">
+                <input type="radio" name="slider" id="item-1" onclick='parking' checked></input>
+                <input type="radio" name="slider" id="item-2" onclick='coffee' checked ></input>
+                <input type="radio" name="slider" id="item-3" onclick='password' checked></input>
                 <div className="cards">
                     <label className="card" for="item-1" id="project-1">
                         <img
-                            src={project1}></img>
+                            src={project1} alt={project1}></img>
                     </label>
                     <label className="card" for="item-2" id="project-2">
                         <img
-                            src={project2}></img>
+                            src={project2} alt={project2}></img>
                     </label>
                     <label className="card" for="item-3" id="project-3">
                         <img
-                            src={project3}></img>
+                            src={project3} alt={project3}></img>
                     </label>
                 </div>
                 <div className="infoContainer" id="infoContainer">
