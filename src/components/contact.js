9ce1086d819887490a3413fdd76ee43aa5d63e09
Noah Hoffman <stacknoah99@gmail.com>
2022-08-01 18:36:47 -0600

    fixed input form on contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,11 +5,18 @@ import emailjs from '@emailjs/browser';
 
 export default function Contact() {
     // Sending form to email 
-    const form = useRef()
+    
 
     const sendEmail = (e) => {
         e.preventDefault();
-        emailjs.send('service_32wky28', 'template_6f7vqrr', form.current, 'nIOQ4DjOD6VPrQRy0')
+        const formData = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            phoneNumber: document.getElementById('phoneNumber').value,
+            message: document.getElementById('message').value
+        }
+        console.log(formData);
+        emailjs.send("service_32wky28","template_6f7vqrr", formData, 'nIOQ4DjOD6VPrQRy0')
         .then((result) => {
             console.log(result.text)
         }, (error) => {
@@ -22,35 +29,36 @@ export default function Contact() {
         <div className="landingPageSection">
             <NavBar />
             <img src={profilePicture} id='avatarPicture' alt="avatar" />
+
             <main className="contactPage">
-                <form ref={form} onSubmit={sendEmail}>
+                <form onSubmit={sendEmail}>
                     <div className="row inputContainer">
                         <div className="colxs12">
                             <div className="styledInput wide">
-                                <input type="text" name="from_name" required />
+                                <input type="text" name="name" required id="name"/>
                                 <label>Name</label>
                             </div>
                         </div>
                         <div className="colmd6 colsm12">
                             <div className="styledInput wide">
-                                <input type="text" name="email" required />
+                                <input type="text" name="email" required id="email" />
                                 <label type="email">Email</label>
                             </div>
                         </div>
                         <div className="colmd6 colsm12">
                             <div className="styledInput wide">
-                                <input type="number" name="phoneNumber" required />
+                                <input type="number" name="phoneNumber" required  id="phoneNumber"/>
                                 <label>Phone Number</label>
                             </div>
                         </div>
                         <div className="colxs12">
                             <div className="styledInput wide">
-                                <textarea required name="message" type="text"></textarea>
+                                <textarea required name="message" type="text" id="message"></textarea>
                                 <label>Message</label>
                             </div>
                         </div>
                         <div className="colxs12">
-                            <button type="submit" value="Send" className="btnlrg submitbtn">Send Message</button>
+                            <button type="submit" className="btnlrg submitbtn">Send Message</button>
                         </div>
                     </div>
                 </form>
